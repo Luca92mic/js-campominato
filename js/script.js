d8038f19162c942fc014b036f55bec2e0c610ac9
Luca <luca.mic92@gmail.com>
2021-06-09 23:42:28 +0200

    fix javascript

--- js/script.js
+++ js/script.js
@@ -1,44 +1,123 @@
-var numeriPc = [];
+var numeriRandom = [];
+var numeriUtente = [];
+var punteggio = 0;
+var possibilita = 84;
+var numeroUtente;
+var trovato = false;
+var livello;
+var numeroMax;
+var numeroMin;
 
-while(numeriPc.length < 16){
-    var numeriRandom = numRandom(1, 100);
-    if(!numeriPc.includes(numRandom)) //numeri non duplicati
-    {
-        numeriPc.push(numRandom);
+
+// chiedo livello difficoltà
+livello = parseInt(prompt("Inserisci il livello di difficoltà: 0, 1 oppure 2"));
+while (livello != 0 && livello != 1 && livello != 2){
+  livello = parseInt(prompt("Per favore inserisci il livello corretto di difficoltà: 0, 1 oppure 2"));
+}
+
+switch (livello) {
+  case 0:
+    numeroMin = 1;
+    numeroMax = 100;
+    possibilita = 84;
+    var titoloDomanda = "Inserisci un numero da 1 a 100";
+    break;
+  case 1:
+    numeroMin = 1;
+    numeroMax = 80;
+    possibilita = 64;
+    titoloDomanda = "Inserisci un numero da 1 a 80";
+    break;
+  case 2:
+    numeroMin = 1;
+    numeroMax = 50;
+    possibilita = 34;
+    titoloDomanda = "Inserisci un numero da 1 a 50";
+    break;
+}
+
+
+// genero numeri random
+while (numeriRandom.length < 16) {
+  //inserisco solo se il numero non è già presente nell'array
+  var numeroCasuale = generaNumeriRandom(1, numeroMax);
+  var cerca = presenteInArray(numeriRandom, numeroCasuale);
+  if (cerca == false) {
+    numeriRandom.push(numeroCasuale);
+  }
+}
+console.log("numeri random " + numeriRandom);
+document.getElementById("numeri-random").innerHTML = numeriRandom;
+
+
+
+while (numeriUtente.length < possibilita && trovato == false) {
+  // chiedo un numero all'utente con un ciclo per verificare che i numeri rispettino il range
+
+  numeroUtente = parseInt(prompt(titoloDomanda));
+  richiediNumeroCorretto();
+  numUgualeInserito()
+
+  if (presenteInArray(numeriUtente, numeroUtente) == false) {
+    numeriUtente.push(numeroUtente);
+    // se il numero dell'utente è presente
+    if (presenteInArray(numeriRandom, numeroUtente) == true) {
+      console.log("partita finita");
+      document.getElementById("messaggio").innerHTML = "Partita finita. Hai beccato il numero nascosto";
+      trovato = true;
+    } else {
+      punteggio++;
     }
-}
-
-console.log(numeriPc);
-
-//chiedere all'utente (100-16) volte di inserire un numero alla volta sempre compreso tra 1 e 100. L’utente non può inserire più volte lo stesso numero.
-
-var arrayUser = [];
-
-//! modificare 20 con 100 alla fine
-while(arrayUser.length < level(100) && !numeriPc.includes(numUser))
-{
-    var numUser = parseInt(prompt('Inserisci un numero da 1 a 100'));
-    if (numeriPc.includes(numUser)){
-        console.log('Hai perso, il tuo risultato è: ' + arrayUser.length);
-    } else if (arrayUser.includes(numUser)){
-        console.log('Hai già inserito il numero, ritenta');
-    } else if(!numeriPc.includes(numUser) && !arrayUser.includes(numUser))
-    {
-        arrayUser.push(numUser);
-    }  
-}
-
-if (arrayUser.length == level(100)) {
-    console.log('Winner');
+  }
 }
 
 
 
-function numRandom(min, max){
-    return Math.floor(Math.random() * (max - min + 1) + min);
+console.log(" è stato trovato? " + trovato);
+console.log("punteggio" + punteggio);
+document.getElementById("punteggio").innerHTML = "Hai totalizzato un punteggio di " + punteggio;
+
+if (numeriUtente.length == possibilita) {
+  document.getElementById("messaggio").innerHTML = "Hai vinto la PARTITA senza lasciarci le penne!";
 }
 
+// FUNZIONI DELLO SCRIPT
+// genero funzione numero random
+function generaNumeriRandom(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
-function level(numMax){
-    return numMax - 16;
+//funzione che controlla che un numero sia in un certo range
+function controlloRangeNumeri(min, max, number) {
+  var result = false;
+  if (number >= min && number <= max) {
+    result = true;
+  }
+  return result;
+}
+
+// funzione che cerca in un array
+function presenteInArray(array, element) {
+  var i = 0;
+  var result = false;
+  while (i < array.length && result == false) {
+    if (array[i] == element) {
+      result = true;
+    }
+    i++;
+  }
+  return result;
+}
+// richiedi numero corretto
+function richiediNumeroCorretto() {
+  while (controlloRangeNumeri(numeroMin, numeroMax, numeroUtente) == false) {
+    numeroUtente = parseInt(prompt("Per favore inserisci un numero corretto: da 1  a " + numeroMax));
+    console.log('Numero inserito: ' + numeroUtente);
+  }
+}
+
+function numUgualeInserito() {
+  while (presenteInArray(numeriUtente, numeroUtente) == true) {
+numeroUtente = parseInt(prompt ("Avevi già inserito questo numero. Riprova con un numero diverso da " + numeroMin + " a " + numeroMax));
+  }
 }
